2ef40234ce49f67a6335baab10bb8be4a0db564e
Adam Skrzypulec <adam.s@northwestern.edu>
2021-11-21 17:51:47 -0600

    reset

--- app/LeetArray.php
+++ app/LeetArray.php
@@ -63,83 +63,8 @@ final class LeetArr
      * https://leetcode.com/problems/two-furthest-houses-with-different-colors/
      *  max distance between two houses with different colors
      */
-    public static function maxDistance($homes)
+    public static function maxDistance($colors)
     {
-        // 1. get all color options
-        print_r($colorsUni = array_unique($homes));
-        $distArr = array_fill(0, count($colorsUni), 0);
-
-        // 2. loop  over each color
-        foreach ($colorsUni as $key => $color)
-        {
-            echo "Color: $color";
-            $firstIndex = array_search($color, $homes); // first occurrence
-            $lastIndex = count($homes) - 1;
-            $dist = 0;
-
-
-            for ($i = $lastIndex; $i > $firstIndex; $i--) {
-                if ($homes[$i-1] != $color) {
-                    $dist = $i - $firstIndex - 1; // find last occurrence
-                    break;
-                }
-//                echo " | Dist: $dist";
-            }
-            echo " | Dist: $dist\n";
-
-
-            $distArr[$key] = $dist;
-        }
-
-        print_r($distArr);
-        print("Max dist: " . max($distArr) . "\n");
-        return max($distArr);
+        return null;
     }
 }
-
-//                    Solution (from the discussion);
-//                    for ($i = 0; $i < count($homes); $i++)
-//                    {
-//                        echo "iteration: $i\n";
-//                        if ($homes[$i] === $homes[$i+1] ) {
-//                              $d++;
-//                              echo "diff color\n";
-//                        } else {
-//                            if ($d >= $r) $r = $d;
-//                        }
-//                        echo "Dist:$d | Result:$r\n";
-//                    }
-//                    $distArr[$key] = $r;
-
-
-//                    if ( 1==1 ) {
-//                    }
-//
-//                    $n = count($colors);
-//                    do {
-//                        $n--;
-//                    } while ($n === 0);
-
-//        for ($i = 0; $i < count($colors); $i++)
-//        {
-//            echo $colors[$i] . " -> " . $colors[$i-1] . "\n";
-//        }
-//        print_r($distArr);
-//        echo(max($distArr) . "\n");
-//        return max($distArr);
-
-//        $distArr = array_fill(0,count($colors), 0);
-//        print_r($colors);
-//        for ($i = 0; $i < count($colors); $i++)
-//        {
-//            echo $colors[$i] . " -> " . $colors[$i-1] . "\n";
-//            if ($colors[$i] == $colors[$i+1]) {
-//                $distArr[$colors[$i]] = 1;
-//            } else {
-//                $distArr[$colors[$i]]++;
-//            }
-//        }
-//        print_r($distArr);
-//        echo(max($distArr) . "\n");
-//        return max($distArr);
-//    }
